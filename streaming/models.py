53e9a05c0c3ab8e609e2f4ff6f0b750901aa60b1
Felix Peters <felix@albrechtpeters.de>
2018-09-26 14:53:59 +0200

    add features to Quote and Reply models

--- streaming/models.py
+++ streaming/models.py
@@ -103,9 +103,25 @@ class Quote():
     def __init__(self, status):
         self.quote_id = status['id']
         self.quoted_status_id = status['quoted_status']['id']
+        self.created_at = status['created_at']
         self.text = status['text']
+        self.lang = status['lang']
+        self.favorite_count = status['favorite_count']
+        self.retweet_count = status['retweet_count']
+        self.hashtag_count = len(status['hashtags'])
+        self.url_count = len(status['urls'])
+        self.mention_count = len(status['user_mentions'])
+        self.media_count = len(status['media'])
         self.user_id = status['user']['id']
         self.user_name = status['user']['screen_name']
+        self.user_created_at = status['user']['created_at']
+        self.user_followers = status['user']['followers']
+        self.user_frieds = status['user']['friends']
+        self.user_statuses = status['user']['statuses_count']
+        self.user_listings = status['user']['listed_count']
+        self.user_verified = status['user']['verified']
+        self.user_favorites = status['user']['favourites_count']
+        self.user_utc_offset = status['user']['utc_offset']
 
     def __repr__(self):
         return colored(self.user_name, 'cyan') + ' ' + self.text
@@ -113,18 +129,50 @@ class Quote():
     def as_tuple(self):
         return (self.quote_id,
                 self.quoted_status_id,
+                self.created_at,
                 self.text,
+                self.lang,
+                self.favorite_count,
+                self.retweet_count,
+                self.hashtag_count,
+                self.url_count,
+                self.mention_count,
+                self.media_count,
                 self.user_id,
-                self.user_name)
+                self.user_name,
+                self.user_created_at,
+                self.user_followers,
+                self.user_frieds,
+                self.user_statuses,
+                self.user_listings,
+                self.user_verified,
+                self.user_favorites,
+                self.user_utc_offset)
 
 class Reply():
 
     def __init__(self, status):
         self.reply_id = status['id']
         self.replied_status_id = status['in_reply_to_status_id']
+        self.created_at = status['created_at']
         self.text = status['text']
+        self.lang = status['lang']
+        self.favorite_count = status['favorite_count']
+        self.retweet_count = status['retweet_count']
+        self.hashtag_count = len(status['hashtags'])
+        self.url_count = len(status['urls'])
+        self.mention_count = len(status['user_mentions'])
+        self.media_count = len(status['media'])
         self.user_id = status['user']['id']
         self.user_name = status['user']['screen_name']
+        self.user_created_at = status['user']['created_at']
+        self.user_followers = status['user']['followers']
+        self.user_frieds = status['user']['friends']
+        self.user_statuses = status['user']['statuses_count']
+        self.user_listings = status['user']['listed_count']
+        self.user_verified = status['user']['verified']
+        self.user_favorites = status['user']['favourites_count']
+        self.user_utc_offset = status['user']['utc_offset']
 
     def __repr__(self):
         return colored(self.user_name, 'magenta') + ' ' + self.text
@@ -132,6 +180,22 @@ class Reply():
     def as_tuple(self):
         return (self.reply_id,
                 self.replied_status_id,
+                self.created_at,
                 self.text,
+                self.lang,
+                self.favorite_count,
+                self.retweet_count,
+                self.hashtag_count,
+                self.url_count,
+                self.mention_count,
+                self.media_count,
                 self.user_id,
-                self.user_name)
+                self.user_name,
+                self.user_created_at,
+                self.user_followers,
+                self.user_frieds,
+                self.user_statuses,
+                self.user_listings,
+                self.user_verified,
+                self.user_favorites,
+                self.user_utc_offset)
