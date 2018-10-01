084460f83c588bb57dfa44626087d04cadf48d71
Felix Peters <felix@albrechtpeters.de>
2018-10-01 11:03:14 +0200

    add backup scripts for tweets database

---
+++ scripts/db_backup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+TIME_STAMP=$(date +"%Y%m%d")
+FILE_NAME="tweet_backup_$TIME_STAMP.sql"
+TMP_DIR="$HOME/tmp_backup"
+FILE_PATH="$TMP_DIR/$FILE_NAME"
+DB_PATH=$1
+S3_BUCKET=$2
+
+mkdir $TMP_DIR
+cp $DB_PATH $FILE_PATH
+aws s3 cp $FILE_PATH $S3_BUCKET
+rm -R $TMP_DIR
+
